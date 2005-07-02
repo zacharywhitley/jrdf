96d004128044024a4c125fd1589a49944c2271b5
nobody <nobody@f2e0c355-1f10-0410-b112-b8ac4569a347>
2005-07-02 15:30:49 +0000

    This commit was manufactured by cvs2svn to create branch 'jrdf0-3-4'.

---
+++ branches/jrdf0-3-4/jrdf/src/org/jrdf/parser/NamespaceListener.java
@@ -0,0 +1,46 @@
+/*  Sesame - Storage and Querying architecture for RDF and RDF Schema
+ *  Copyright (C) 2001-2004 Aduna
+ *
+ *  Contact:
+ *  	Aduna
+ *  	Prinses Julianaplein 14 b
+ *  	3817 CS Amersfoort
+ *  	The Netherlands
+ *  	tel. +33 (0)33 465 99 87
+ *  	fax. +33 (0)33 465 99 87
+ *
+ *  	http://aduna.biz/
+ *  	http://www.openrdf.org/
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public
+ *  License as published by the Free Software Foundation; either
+ *  version 2.1 of the License, or (at your option) any later version.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this library; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+package org.jrdf.parser;
+
+/**
+ * An interface defining methods for receiving namespace declarations from
+ * an RDF parser.
+ **/
+public interface NamespaceListener {
+
+  /**
+   * Called by an RDF parser when it has encountered a new namespace
+   * declaration.
+   *
+   * @param prefix The prefix that is used in the namespace declaration.
+   * @param uri The URI of the namespace.
+   **/
+  void handleNamespace(String prefix, String uri);
+}
