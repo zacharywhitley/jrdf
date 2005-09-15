f5e9c4f13a9455c587c5a1431db1fad74025a588
newmana <newmana@f2e0c355-1f10-0410-b112-b8ac4569a347>
2005-09-15 11:37:57 +0000

    Son of ploppy.

--- trunk/jrdf/test/java/org/jrdf/util/test/runner/IntegrationTestRunner.java
+++ trunk/jrdf/test/java/org/jrdf/util/test/runner/IntegrationTestRunner.java
@@ -9,12 +9,11 @@ import java.io.IOException;
  * Run all the unit tests in the project.
  *
  * @author Andrew Newman
- *
  * @version $Revision$
  */
 public class IntegrationTestRunner extends TestCase {
-  public static Test suite() throws IOException {
-    return new DefaultTestRunner("IntegrationTest");
-  }
+    public static Test suite() throws IOException {
+        return new DefaultTestRunner("IntegrationTest");
+    }
 }
 
--- trunk/jrdf/test/java/org/jrdf/util/test/runner/UnitTestRunner.java
+++ trunk/jrdf/test/java/org/jrdf/util/test/runner/UnitTestRunner.java
@@ -1,28 +1,18 @@
 package org.jrdf.util.test.runner;
 
-import com.gargoylesoftware.base.testing.RecursiveTestSuite;
-import com.gargoylesoftware.base.testing.TestFilter;
-import com.gargoylesoftware.base.testing.AcceptAllTestFilter;
+import junit.framework.Test;
+import junit.framework.TestCase;
 
 import java.io.IOException;
-import java.io.File;
-import java.lang.reflect.Modifier;
-
-import junit.framework.TestSuite;
-import junit.framework.Test;
-import junit.framework.TestResult;
-import junit.framework.TestCase;
 
 /**
  * Run all the unit tests in the project.
  *
  * @author Andrew Newman
- *
  * @version $Revision$
  */
 public class UnitTestRunner extends TestCase {
-  public static Test suite() throws IOException {
-    return new DefaultTestRunner("UnitTest");
-  }
+    public static Test suite() throws IOException {
+        return new DefaultTestRunner("UnitTest");
+    }
 }
-
