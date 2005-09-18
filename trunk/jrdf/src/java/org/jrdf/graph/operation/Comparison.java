daa8cb6b0e492248d80a37b386388ef5fd6fbeac
newmana <newmana@f2e0c355-1f10-0410-b112-b8ac4569a347>
2005-09-18 14:36:31 +0000

    First add.

--- trunk/jrdf/src/java/org/jrdf/graph/operation/Comparison.java
+++ trunk/jrdf/src/java/org/jrdf/graph/operation/Comparison.java
@@ -21,7 +21,7 @@ public interface Comparison {
      * Return true if both graphs are equivalent (isomorphic) to one another.  That is, that the nodes in one graph map
      * equivalently to nodes in the other.  In a non-grounded graph (ones with blank nodes) nodes can map to other
      * nodes with different values but are equivalent.  For example, <a>, <b>, <c> is equivalient to _x, <b>, <c>, where
-     * _a is a blank node.
+     * _x is a blank node.
      *
      * @param g1 The first graph to test.
      * @param g2 The second graph to test.
