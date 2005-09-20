67ea7dd2e38596f6ce9377d7ed5b81be8d127fab
newmana <newmana@f2e0c355-1f10-0410-b112-b8ac4569a347>
2005-09-20 07:23:32 +0000

    Added standard header

---
+++ jrdf/test/java/org/jrdf/graph/mem/operation/ComparisonUnitTest.java
@@ -0,0 +1,11 @@
+package org.jrdf.graph.mem.operation;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: andrew
+ * Date: Sep 20, 2005
+ * Time: 5:18:40 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class ComparisonUnitTest {
+}
