82f2f9ad52d1e4f5ddafe484b0598de8bb3384e9
newmana <newmana@f2e0c355-1f10-0410-b112-b8ac4569a347>
2005-10-01 11:32:39 +0000

    Refactor.

--- trunk/jrdf/src/java/org/jrdf/graph/operation/Difference.java
+++ trunk/jrdf/src/java/org/jrdf/graph/operation/Difference.java
@@ -61,8 +61,8 @@ package org.jrdf.graph.operation;
 import org.jrdf.graph.Graph;
 
 /**
- * The set difference between two graphs - all the elements that belong in
- * the first graph that do not belong in the second.
+ * The set difference between two graphs - all the elements that belong in the first graph that do not belong in the
+ * second.
  *
  * @author Andrew Newman
  * @version $Revision$
