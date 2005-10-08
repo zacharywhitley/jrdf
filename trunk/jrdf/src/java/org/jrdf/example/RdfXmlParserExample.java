c8fe24d2f998b721498ff11bcbb5f3c09a2cf626
newmana <newmana@f2e0c355-1f10-0410-b112-b8ac4569a347>
2005-10-08 10:19:51 +0000

    Ported bug fixes back to head, changed release notes.

--- trunk/jrdf/src/java/org/jrdf/example/RdfXmlParserExample.java
+++ trunk/jrdf/src/java/org/jrdf/example/RdfXmlParserExample.java
@@ -1,8 +1,8 @@
 package org.jrdf.example;
 
-import static org.jrdf.graph.AnyObjectNode.*;
-import static org.jrdf.graph.AnyPredicateNode.*;
-import static org.jrdf.graph.AnySubjectNode.*;
+import static org.jrdf.graph.AnyObjectNode.ANY_OBJECT_NODE;
+import static org.jrdf.graph.AnyPredicateNode.ANY_PREDICATE_NODE;
+import static org.jrdf.graph.AnySubjectNode.ANY_SUBJECT_NODE;
 import org.jrdf.graph.Graph;
 import org.jrdf.graph.ObjectNode;
 import org.jrdf.graph.PredicateNode;
@@ -12,30 +12,52 @@ import org.jrdf.parser.StatementHandler;
 import org.jrdf.parser.rdfxml.RdfXmlParser;
 
 import java.io.InputStream;
+import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.Iterator;
 
 public class RdfXmlParserExample {
+    private static final String DEFAULT_RDF_URL = "http://rss.slashdot.org/Slashdot/slashdot";
+
     public static void main(String[] args) throws Exception {
-        String baseURI = "http://rss.slashdot.org/Slashdot/slashdot";
-        URL url = new URL(baseURI);
+        URL url = getDocumentURL(args);
         InputStream is = url.openStream();
         final Graph jrdfMem = new GraphImpl();
         RdfXmlParser parser = new RdfXmlParser(jrdfMem.getElementFactory());
-        parser.setStatementHandler(new StatementHandler() {
-            public void handleStatement(SubjectNode subject, PredicateNode predicate, ObjectNode object) {
-                try {
-                    jrdfMem.add(subject, predicate, object);
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
-            }
-        });
-        parser.parse(is, baseURI);
+        ExampleStatementHandler sh = new ExampleStatementHandler(jrdfMem);
+        parser.setStatementHandler(sh);
+        parser.parse(is, url.toURI().toString());
         Iterator iter = jrdfMem.find(ANY_SUBJECT_NODE, ANY_PREDICATE_NODE, ANY_OBJECT_NODE);
         while (iter.hasNext()) {
-            System.err.println("Graph: " + iter.next());
+            System.out.println("Graph: " + iter.next());
         }
         is.close();
+    }
+
+    private static URL getDocumentURL(String[] args) throws MalformedURLException {
+        String baseURL;
+        if (args.length == 0 || args[0].length() == 0) {
+            System.out.println("First argument empty so using: " + DEFAULT_RDF_URL);
+            baseURL = DEFAULT_RDF_URL;
+        } else {
+            baseURL = args[0];
+        }
+        return new URL(baseURL);
+    }
+
+    private static class ExampleStatementHandler implements StatementHandler {
+        private final Graph jrdfMem;
+
+        public ExampleStatementHandler(Graph jrdfMem) {
+            this.jrdfMem = jrdfMem;
+        }
+
+        public void handleStatement(SubjectNode subject, PredicateNode predicate, ObjectNode object) {
+            try {
+                jrdfMem.add(subject, predicate, object);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
