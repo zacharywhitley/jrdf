a4e03052702a6ff17ce800fb7f5d9803aea3c8e9
newmana <newmana@f2e0c355-1f10-0410-b112-b8ac4569a347>
2006-06-21 06:26:49 +0000

    Re-enabled unit test.

--- jrdf/test/java/org/jrdf/connection/JrdfConnectionFactoryUnitTest.java
+++ jrdf/test/java/org/jrdf/connection/JrdfConnectionFactoryUnitTest.java
@@ -1,12 +1,14 @@
 package org.jrdf.connection;
 
 import junit.framework.TestCase;
-import org.jrdf.graph.Graph;
-import org.jrdf.query.JrdfQueryExecutor;
+import org.jrdf.query.JrdfQueryExecutorFactory;
 import org.jrdf.query.QueryBuilder;
+import org.jrdf.sparql.SparqlConnection;
+import org.jrdf.util.test.AssertThrows;
 import org.jrdf.util.test.MockTestUtil;
 
 import java.net.URI;
+import java.net.URL;
 
 /**
  * Unit test for {@link JrdfConnectionFactory}.
@@ -17,58 +19,54 @@ import java.net.URI;
 public class JrdfConnectionFactoryUnitTest extends TestCase {
 
     private static final String EXPECTED_NO_SECURITY_DOMAIN = "http://jrdf.sf.net/connection#NO_SECURITY";
-    private static final Graph MOCK_GRAPH = MockTestUtil.createMock(Graph.class);
-    private static final URI NO_SECURITY_DOMAIN = JrdfConnectionFactory.NO_SECURITY_DOMAIN;
-    private static final JrdfQueryExecutor QUERY_EXECUTOR = MockTestUtil.createMock(JrdfQueryExecutor.class);
+    private static final URL NO_SECURITY_DOMAIN = JrdfConnectionFactory.NO_SECURITY_DOMAIN_URL;
+    private static final JrdfQueryExecutorFactory QUERY_EXECUTOR_FACTORY
+            = MockTestUtil.createMock(JrdfQueryExecutorFactory.class);
     private static final QueryBuilder QUERY_BUILDER = MockTestUtil.createMock(QueryBuilder.class);
 
-    // TODO (AN) !! Come back a fix me!!
-    public void testBadMan() {
+    public void testNoSecurityConstant() {
+        assertEquals(URI.create(EXPECTED_NO_SECURITY_DOMAIN), JrdfConnectionFactory.NO_SECURITY_DOMAIN);
     }
 
-//    public void testNoSecurityConstant() {
-//        assertEquals(URI.create(EXPECTED_NO_SECURITY_DOMAIN), JrdfConnectionFactory.NO_SECURITY_DOMAIN);
-//    }
-//
-//    public void testNullSecurityDomainThrowsException() {
-//        AssertThrows.assertThrows(IllegalArgumentException.class, new AssertThrows.Block() {
-//            public void execute() throws Throwable {
-//                createFactory().createSparqlConnection(null, QUERY_EXECUTOR, QUERY_BUILDER);
-//            }
-//        });
-//    }
-//
-//    public void testNullQueryExecutorThrowsException() {
-//        AssertThrows.assertThrows(IllegalArgumentException.class, new AssertThrows.Block() {
-//            public void execute() throws Throwable {
-//                createFactory().createSparqlConnection(NO_SECURITY_DOMAIN, null, QUERY_BUILDER);
-//            }
-//        });
-//    }
-//
-//    public void testNullQueryBuilderThrowsException() {
-//        AssertThrows.assertThrows(IllegalArgumentException.class, new AssertThrows.Block() {
-//            public void execute() throws Throwable {
-//                createFactory().createSparqlConnection(NO_SECURITY_DOMAIN, QUERY_EXECUTOR, null);
-//            }
-//        });
-//    }
-//
-//    public void testGeSparqlConnection() {
-//        assertNotNull(createConnectionWithBadGraph());
-//    }
-//
-//    public void testCreateConnectionReturnsNewConnectionEachTime() {
-//        SparqlConnection connection1 = createConnectionWithBadGraph();
-//        SparqlConnection connection2 = createConnectionWithBadGraph();
-//        assertNotSame(connection1, connection2);
-//    }
-//
-//    private SparqlConnection createConnectionWithBadGraph() {
-//        return createFactory().createSparqlConnection(NO_SECURITY_DOMAIN, QUERY_EXECUTOR, QUERY_BUILDER);
-//    }
-//
-//    private JrdfConnectionFactory createFactory() {
-//        return new JrdfConnectionFactory();
-//    }
+    public void testNullSecurityDomainThrowsException() {
+        AssertThrows.assertThrows(IllegalArgumentException.class, new AssertThrows.Block() {
+            public void execute() throws Throwable {
+                createFactory().createSparqlConnection(null, QUERY_BUILDER, QUERY_EXECUTOR_FACTORY);
+            }
+        });
+    }
+
+    public void testNullQueryExecutorThrowsException() {
+        AssertThrows.assertThrows(IllegalArgumentException.class, new AssertThrows.Block() {
+            public void execute() throws Throwable {
+                createFactory().createSparqlConnection(NO_SECURITY_DOMAIN, null, QUERY_EXECUTOR_FACTORY);
+            }
+        });
+    }
+
+    public void testNullQueryBuilderThrowsException() {
+        AssertThrows.assertThrows(IllegalArgumentException.class, new AssertThrows.Block() {
+            public void execute() throws Throwable {
+                createFactory().createSparqlConnection(NO_SECURITY_DOMAIN, QUERY_BUILDER, null);
+            }
+        });
+    }
+
+    public void testGeSparqlConnection() {
+        assertNotNull(createConnectionWithBadGraph());
+    }
+
+    public void testCreateConnectionReturnsNewConnectionEachTime() {
+        SparqlConnection connection1 = createConnectionWithBadGraph();
+        SparqlConnection connection2 = createConnectionWithBadGraph();
+        assertNotSame(connection1, connection2);
+    }
+
+    private SparqlConnection createConnectionWithBadGraph() {
+        return createFactory().createSparqlConnection(NO_SECURITY_DOMAIN, QUERY_BUILDER, QUERY_EXECUTOR_FACTORY);
+    }
+
+    private JrdfConnectionFactory createFactory() {
+        return new JrdfConnectionFactory();
+    }
 }
