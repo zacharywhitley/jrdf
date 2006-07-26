c72296b775fcbd39019f7f0038ae4cde0615183e
newmana <newmana@f2e0c355-1f10-0410-b112-b8ac4569a347>
2006-07-26 13:53:40 +0000

    Fixed join code.

--- jrdf/src/java/org/jrdf/query/relation/operation/mem/join/outer/FullOuterJoinImpl.java
+++ jrdf/src/java/org/jrdf/query/relation/operation/mem/join/outer/FullOuterJoinImpl.java
@@ -75,7 +75,8 @@ public class FullOuterJoinImpl implements DyadicJoin {
     }
 
     // TODO (AN) Add tuple subsumption i.e. go through the results and remove the results for a given attribute binding
-    // that have null values.  Or add it to a new time of join.
+    // that have null values.  Or add it to a new time of join.  Or replace this with minimum union and tuple
+    // subsumption.
 
     public Relation join(Relation relation1, Relation relation2) {
         Relation leftOuterJoinResult = leftOuterJoin.join(relation1, relation2);
