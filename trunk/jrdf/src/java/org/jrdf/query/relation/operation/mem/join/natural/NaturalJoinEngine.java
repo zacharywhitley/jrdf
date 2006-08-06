536d18796e9ed5dfd8f3d796f7e8fd80f1dbf7e4
newmana <newmana@f2e0c355-1f10-0410-b112-b8ac4569a347>
2006-08-06 01:32:17 +0000

    Moved unions around and implemented MinimumUnion

--- trunk/jrdf/src/java/org/jrdf/query/relation/operation/mem/join/natural/NaturalJoinEngine.java
+++ trunk/jrdf/src/java/org/jrdf/query/relation/operation/mem/join/natural/NaturalJoinEngine.java
@@ -77,7 +77,7 @@ import java.util.TreeSet;
  * <p/>
  * The general algorithm is:
  * 1. Find all matching attributes on two relations.
- * 2. UnionImpl the attributes and tuples together.
+ * 2. OuterUnionImpl the attributes and tuples together.
  * 3. Remove any attributes that are not common between the two.
  * <p/>
  * For example:
