123722dd312239aa076adf012779d5a6e6431122
newmana <newmana@f2e0c355-1f10-0410-b112-b8ac4569a347>
2006-08-13 20:26:50 +0000

    Fixed bug with URL encoding and loading multiple models.

--- jrdf/src/java/org/jrdf/example/RdfXmlParserExample.java
+++ jrdf/src/java/org/jrdf/example/RdfXmlParserExample.java
@@ -7,11 +7,11 @@ import static org.jrdf.graph.AnySubjectNode.ANY_SUBJECT_NODE;
 import org.jrdf.graph.Graph;
 import org.jrdf.parser.Parser;
 import org.jrdf.parser.rdfxml.GraphRdfXmlParser;
+import org.jrdf.util.EscapeURL;
 
 import java.io.InputStream;
 import java.net.MalformedURLException;
 import java.net.URL;
-import java.net.URI;
 import java.util.Iterator;
 
 /**
@@ -29,9 +29,7 @@ public final class RdfXmlParserExample {
         InputStream in = url.openStream();
         final Graph jrdfMem = getGraph();
         Parser parser = new GraphRdfXmlParser(jrdfMem);
-        // TODO: AN Replace with proper URL encoding!
-        URI uri =  new URI(url.toString().replace(" ", "%20"));
-        parser.parse(in, uri.toString());
+        parser.parse(in, EscapeURL.toEscapedString(url));
         Iterator iter = jrdfMem.find(ANY_SUBJECT_NODE, ANY_PREDICATE_NODE, ANY_OBJECT_NODE);
         while (iter.hasNext()) {
             System.out.println("Graph: " + iter.next());
