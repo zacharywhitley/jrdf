8046eca1288162354631d31f2821b4c0955afbf5
newmana <newmana@f2e0c355-1f10-0410-b112-b8ac4569a347>
2007-01-05 04:56:09 +0000

    Updated copyright

--- trunk/jrdf/src/java/org/jrdf/graph/mem/iterator/OneFixedIterator.java
+++ trunk/jrdf/src/java/org/jrdf/graph/mem/iterator/OneFixedIterator.java
@@ -7,7 +7,7 @@
  *
  * The Apache Software License, Version 1.1
  *
- * Copyright (c) 2003, 2004 The JRDF Project.  All rights reserved.
+ * Copyright (c) 2003-2007 The JRDF Project.  All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -54,6 +54,7 @@
  * This software consists of voluntary contributions made by many
  * individuals on behalf of the JRDF Project.  For more
  * information on JRDF, please see <http://jrdf.sourceforge.net/>.
+ *
  */
 
 package org.jrdf.graph.mem.iterator;
--- trunk/jrdf/test/java/org/jrdf/sparql/parser/SableCcSparqlParserUnitTest.java
+++ trunk/jrdf/test/java/org/jrdf/sparql/parser/SableCcSparqlParserUnitTest.java
@@ -1,13 +1,13 @@
 /*
  * $Header$
- * $Revision: 921 $
- * $Date: 2006-10-31 19:52:43 +1000 (Tue, 31 Oct 2006) $
+ * $Revision: 982 $
+ * $Date: 2006-12-08 18:42:51 +1000 (Fri, 08 Dec 2006) $
  *
  * ====================================================================
  *
  * The Apache Software License, Version 1.1
  *
- * Copyright (c) 2003-2005 The JRDF Project.  All rights reserved.
+ * Copyright (c) 2003-2007 The JRDF Project.  All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -54,6 +54,7 @@
  * This software consists of voluntary contributions made by many
  * individuals on behalf of the JRDF Project.  For more
  * information on JRDF, please see <http://jrdf.sourceforge.net/>.
+ *
  */
 
 package org.jrdf.sparql.parser;
