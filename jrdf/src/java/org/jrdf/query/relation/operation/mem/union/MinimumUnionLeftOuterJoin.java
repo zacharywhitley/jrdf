d7381fe2707d0aeac6650ae39e99e35b01baab79
newmana <newmana@f2e0c355-1f10-0410-b112-b8ac4569a347>
2007-01-26 05:34:01 +0000

    Reduced duplication.

---
+++ jrdf/src/java/org/jrdf/query/relation/operation/mem/union/MinimumUnionLeftOuterJoin.java
@@ -0,0 +1,9 @@
+package org.jrdf.query.relation.operation.mem.union;
+
+import org.jrdf.query.relation.operation.DyadicJoin;
+
+/**
+ * Indicates a special kind of DyadicJoin.
+ */
+public interface MinimumUnionLeftOuterJoin extends DyadicJoin {
+}
