eb263f039931c19bf813fbfd15ca423eb1cf75e6
newmana <newmana@f2e0c355-1f10-0410-b112-b8ac4569a347>
2007-01-28 22:52:53 +0000

    NTriples start.

---
+++ trunk/jrdf/test/java/org/jrdf/parser/ntriples/NTripleIntegrationTest.java
@@ -0,0 +1,88 @@
+/*
+ * $Header$
+ * $Revision: 982 $
+ * $Date: 2006-12-08 18:42:51 +1000 (Fri, 08 Dec 2006) $
+ *
+ * ====================================================================
+ *
+ * The Apache Software License, Version 1.1
+ *
+ * Copyright (c) 2003-2007 The JRDF Project.  All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in
+ *    the documentation and/or other materials provided with the
+ *    distribution.
+ *
+ * 3. The end-user documentation included with the redistribution, if
+ *    any, must include the following acknowlegement:
+ *       "This product includes software developed by the
+ *        the JRDF Project (http://jrdf.sf.net/)."
+ *    Alternately, this acknowlegement may appear in the software itself,
+ *    if and wherever such third-party acknowlegements normally appear.
+ *
+ * 4. The names "The JRDF Project" and "JRDF" must not be used to endorse
+ *    or promote products derived from this software without prior written
+ *    permission. For written permission, please contact
+ *    newmana@users.sourceforge.net.
+ *
+ * 5. Products derived from this software may not be called "JRDF"
+ *    nor may "JRDF" appear in their names without prior written
+ *    permission of the JRDF Project.
+ *
+ * THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESSED OR IMPLIED
+ * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED.  IN NO EVENT SHALL THE APACHE SOFTWARE FOUNDATION OR
+ * ITS CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF
+ * USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT
+ * OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ * ====================================================================
+ *
+ * This software consists of voluntary contributions made by many
+ * individuals on behalf of the JRDF Project.  For more
+ * information on JRDF, please see <http://jrdf.sourceforge.net/>.
+ *
+ */
+
+package org.jrdf.parser.ntriples;
+
+import junit.framework.TestCase;
+
+import java.io.InputStream;
+import java.io.IOException;
+import java.io.PushbackReader;
+import java.io.InputStreamReader;
+import java.net.URL;
+
+import org.jrdf.sparql.parser.lexer.Lexer;
+import org.jrdf.sparql.parser.parser.Parser;
+
+public class NTripleIntegrationTest extends TestCase {
+    private static final String TEST_DATA = "org/jrdf/parser/ntriples/test.nt";
+    private static final int PUSHBACK_BUFFER_SIZE = 256;
+
+    public void testParseFile() throws IOException {
+        InputStream in = getSampleDate();
+        PushbackReader reader = new PushbackReader(new InputStreamReader(in), PUSHBACK_BUFFER_SIZE);
+        Lexer lexer = new Lexer(reader);
+        Parser parser = new Parser(lexer);
+    }
+    
+    public InputStream getSampleDate() throws IOException {
+        URL source = getClass().getClassLoader().getResource(TEST_DATA);
+        return source.openStream();
+    }
+}
