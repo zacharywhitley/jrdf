9d93aaf868a6d19b0a6f4b26e5185cbca774f38f
newmana <newmana@f2e0c355-1f10-0410-b112-b8ac4569a347>
2007-07-10 22:44:45 +0000

    Change type hierarchy.

--- trunk/jrdf/src/java/org/jrdf/graph/Resource.java
+++ trunk/jrdf/src/java/org/jrdf/graph/Resource.java
@@ -4,7 +4,7 @@ package org.jrdf.graph;
  * A resource stands for either a Blank Node or a URI Reference.  This is a convienence interface designed to make it
  * easier to create triples programmatically.
  */
-public interface Resource extends URIReference, BlankNode {
+public interface Resource extends SubjectNode, ObjectNode {
     boolean isURIReference();
 //    void addValue(URI uri, String lexicalValue);
 //    void addValue(URI uri, String lexicalValue, String languageType);
