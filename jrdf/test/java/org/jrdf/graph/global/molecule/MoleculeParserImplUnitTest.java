e7a2ded2254ac4e2b970d06c552fe8f85524114f
newmana <newmana@f2e0c355-1f10-0410-b112-b8ac4569a347>
2007-09-26 03:21:10 +0000

    Changed name of test.

--- jrdf/test/java/org/jrdf/graph/global/molecule/MoleculeParserImplUnitTest.java
+++ jrdf/test/java/org/jrdf/graph/global/molecule/MoleculeParserImplUnitTest.java
@@ -91,8 +91,8 @@ public class MoleculeParserImplUnitTest extends TestCase {
 //        URL resource = getClass().getResource("/org/jrdf/example/pizza.rdf");
 //        moleculeParser.parse(resource.openStream(), EscapeURL.toEscapedString(resource));
 //        GlobalizedGraph globalizedGraph = moleculeParser.getGlobalizedGraph();
-//        assertEquals(NUMBER_OF_MOLECULES_IN_PIZZA, globalizedGraph.getNumberOfMolecules());
 //        assertEquals(NUMBER_OF_TRIPLES_IN_PIZZA, globalizedGraph.getNumberOfTriples());
+//        assertEquals(NUMBER_OF_MOLECULES_IN_PIZZA, globalizedGraph.getNumberOfMolecules());
 //        Triple triple = jrdfGraph.getTripleFactory()
 //            .addTriple(URI.create("http://www.co-ode.org/ontologies/pizza/pizza.owl#American"),
 //                RDF.TYPE, URI.create("http://www.w3.org/2002/07/owl#Class"));
