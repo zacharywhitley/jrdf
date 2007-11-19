1629dddc939053de816697089e03fc43d188a512
newmana <newmana@f2e0c355-1f10-0410-b112-b8ac4569a347>
2007-11-19 04:21:00 +0000

    Adding non-memory bound implementations of iterators.

--- trunk/jrdf/src/java/org/jrdf/set/SetFactory.java
+++ trunk/jrdf/src/java/org/jrdf/set/SetFactory.java
@@ -2,13 +2,6 @@ package org.jrdf.set;
 
 import java.util.Set;
 
-/**
- * Created by IntelliJ IDEA.
- * User: liyf
- * Date: Nov 19, 2007
- * Time: 10:30:13 AM
- * To change this template use File | Settings | File Templates.
- */
 public interface SetFactory {
     <T> Set<T> createSet(Class<T> clazz);
 
