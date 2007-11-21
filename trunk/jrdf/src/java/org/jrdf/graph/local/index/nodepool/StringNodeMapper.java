a690d4229bc6e33723644cea5ce9a5e8ae69dfdf
newmana <newmana@f2e0c355-1f10-0410-b112-b8ac4569a347>
2007-11-21 04:09:36 +0000

    Removed suppression.

--- trunk/jrdf/src/java/org/jrdf/graph/local/index/nodepool/StringNodeMapper.java
+++ trunk/jrdf/src/java/org/jrdf/graph/local/index/nodepool/StringNodeMapper.java
@@ -3,9 +3,10 @@ package org.jrdf.graph.local.index.nodepool;
 import org.jrdf.graph.BlankNode;
 import org.jrdf.graph.Literal;
 import org.jrdf.graph.Node;
+import org.jrdf.graph.TypedNodeVisitor;
 import org.jrdf.graph.URIReference;
 
-public interface StringNodeMapper {
+public interface StringNodeMapper extends TypedNodeVisitor {
     String convertToString(Node node);
 
     BlankNode convertToBlankNode(String string);
