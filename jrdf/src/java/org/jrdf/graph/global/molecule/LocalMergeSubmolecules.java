1be1964cf6bcad2f6721cf47ebb3104c01d58c1e
newmana <newmana@f2e0c355-1f10-0410-b112-b8ac4569a347>
2007-12-15 02:27:52 +0000

    Started merger.

---
+++ jrdf/src/java/org/jrdf/graph/global/molecule/LocalMergeSubmolecules.java
@@ -0,0 +1,16 @@
+package org.jrdf.graph.global.molecule;
+
+import org.jrdf.graph.BlankNode;
+import org.jrdf.graph.Triple;
+
+import java.util.Map;
+
+/**
+ * Class description goes here.
+ */
+public interface LocalMergeSubmolecules {
+    NewMolecule merge(NewMolecule molecule1, NewMolecule molecule2, Map<BlankNode, BlankNode> map);
+
+    NewMolecule merge(Triple currentTriple, NewMolecule molecule1, NewMolecule molecule2,
+            Map<BlankNode, BlankNode> map);
+}
