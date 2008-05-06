82a9288384239795348ac5f9bfceb53cfa72e76a
newmana <newmana@f2e0c355-1f10-0410-b112-b8ac4569a347>
2008-05-06 06:38:56 +0000

    Bugger.

--- jrdf/src/java/org/jrdf/graph/global/molecule/mem/NewNaiveGraphDecomposerImpl.java
+++ jrdf/src/java/org/jrdf/graph/global/molecule/mem/NewNaiveGraphDecomposerImpl.java
@@ -100,7 +100,9 @@ public class NewNaiveGraphDecomposerImpl implements NewGraphDecomposer {
                 NewMolecule molecule = moleculeFactory.createMolecue();
                 molecule = molecule.add(currentTriple);
                 triplesChecked.add(currentTriple);
+                System.err.println("Molecule before " + molecule);
                 molecule = convertTripleToMolecule(molecule);
+                System.err.println("Molecule after " + molecule);
                 molecules.add(molecule);
             }
         }
@@ -131,6 +133,7 @@ public class NewNaiveGraphDecomposerImpl implements NewGraphDecomposer {
         throws GraphException {
         while (closableIterator.hasNext()) {
             Triple triple = closableIterator.next();
+            System.err.println("Triple: " + triple);
             if (!triplesChecked.contains(triple)) {
                 if (isDoubleLinkedTriple(triple)) {
                     molecule = addLinkedTriple(molecule, triple);
@@ -149,6 +152,9 @@ public class NewNaiveGraphDecomposerImpl implements NewGraphDecomposer {
         subMolecule.add(triple);
         triplesChecked.add(triple);
         getSubMolecule(subMolecule);
+        System.err.println("Molecule " + molecule);
+        System.err.println("Triple " + triple);
+        System.err.println("Submolecule " + subMolecule);
         // Put submolecule inside molecule's head triple
         if (isDoubleLinkedTriple(molecule.getHeadTriple()) &&
             molecule.getHeadTriple().getObject().equals(triple.getSubject())) {
