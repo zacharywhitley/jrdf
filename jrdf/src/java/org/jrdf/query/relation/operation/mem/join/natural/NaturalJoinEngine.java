da3c7fc2a830faec344c80a33422de3a35501505
newmana <newmana@f2e0c355-1f10-0410-b112-b8ac4569a347>
2008-07-28 00:27:44 +0000

    Cleaned it up a little.

--- jrdf/src/java/org/jrdf/query/relation/operation/mem/join/natural/NaturalJoinEngine.java
+++ jrdf/src/java/org/jrdf/query/relation/operation/mem/join/natural/NaturalJoinEngine.java
@@ -81,18 +81,6 @@ import java.util.TreeSet;
  * 2. Union the attributes and tuples together.
  * 3. Remove any attributes that are not common between the two.
  * <p/>
- * For example:
- * Relation 1 has the following statements: <1, a, foo>, <1, b, bar>,
- * <1, c, bar>, <1, c, baz>
- * Relation 2 has the following statements: <2, b, foo>, <2, c, bar>,
- * <2, f, bar>, <2, g, bar>, <2, c, baz>, <2, f, baz>
- * <p/>
- * After process:
- * <2, b, bar>, <2, c, bar>, <1, c, bar>, <1, f, bar>, <1, f, bar>, <2, c, baz>,
- * <1, c, baz>, <1, f, baz>
- * Removed:
- * <2, a, foo>, <1, a, foo>
-
  */
 public class NaturalJoinEngine implements TupleEngine {
     private final TupleFactory tupleFactory;
