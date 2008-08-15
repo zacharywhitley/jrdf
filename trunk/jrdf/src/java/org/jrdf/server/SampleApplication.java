6b75ffd657508789f9f20defe3a7f2be0b9e52f8
newmana <newmana@f2e0c355-1f10-0410-b112-b8ac4569a347>
2008-08-15 06:14:11 +0000

    Adding Restlet.

--- trunk/jrdf/src/java/org/jrdf/server/SampleApplication.java
+++ trunk/jrdf/src/java/org/jrdf/server/SampleApplication.java
@@ -4,16 +4,23 @@ import org.restlet.Application;
 import org.restlet.Context;
 import org.restlet.Restlet;
 import org.restlet.Router;
+import org.jrdf.util.DirectoryHandler;
+import org.jrdf.util.TempDirectoryHandler;
+import org.jrdf.PersistentGlobalJRDFFactory;
+import static org.jrdf.PersistentGlobalJRDFFactoryImpl.getFactory;
 
 public class SampleApplication extends Application {
-   public SampleApplication(Context parentContext) {
-      super(parentContext);
-   }
+    private static final DirectoryHandler HANDLER = new TempDirectoryHandler();
+    private static final PersistentGlobalJRDFFactory FACTORY = getFactory(HANDLER);
 
-   @Override
-   public synchronized Restlet createRoot() {
-      Router router = new Router(getContext());
-      router.attachDefault(HelloWorldResource.class);
-      return router;
-   }
+    public SampleApplication(Context parentContext) {
+        super(parentContext);
+    }
+
+    @Override
+    public synchronized Restlet createRoot() {
+        Router router = new Router(getContext());
+        router.attachDefault(GraphResource.class);
+        return router;
+    }
 }
