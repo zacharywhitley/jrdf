f05ab09dc5a8ba46cd0969b5c108fd8b1e551734
newmana <newmana@f2e0c355-1f10-0410-b112-b8ac4569a347>
2008-10-06 11:43:49 +0000

    Fixing build.

--- jrdf/build.sh
+++ jrdf/build.sh
@@ -46,9 +46,9 @@ fi
 JAVA_BIN=${JAVA_HOME}/bin/java
 
 # Add the ant libraries to the classpath
-CLASSPATH=${CLASSPATH}:${LIBDIR}/ant/ant-1.6.5.jar
-CLASSPATH=${CLASSPATH}:${LIBDIR}/ant/ant-launcher-1.6.5.jar
-CLASSPATH=${CLASSPATH}:${LIBDIR}/ant/ant-junit-1.6.5.jar
+CLASSPATH=${CLASSPATH}:${LIBDIR}/ant/ant-1.7.1.jar
+CLASSPATH=${CLASSPATH}:${LIBDIR}/ant/ant-launcher-1.7.1.jar
+CLASSPATH=${CLASSPATH}:${LIBDIR}/ant/ant-junit-1.7.1.jar
 CLASSPATH=${CLASSPATH}:${LIBDIR}/junit-3.8.1.jar
 
 # Try to include tools.jar for compilation
