e543412699b48111581977fae610e612235a4656
newmana <newmana@f2e0c355-1f10-0410-b112-b8ac4569a347>
2008-11-23 22:36:29 +0000

    Refactoring parsing NTriple/N3 code.

--- jrdf/src/java/org/jrdf/parser/n3/PrefixParserImpl.java
+++ jrdf/src/java/org/jrdf/parser/n3/PrefixParserImpl.java
@@ -1,16 +1,16 @@
 package org.jrdf.parser.n3;
 
-import org.jrdf.util.boundary.RegexMatcherFactory;
-import org.jrdf.util.boundary.RegexMatcher;
 import org.jrdf.parser.NamespaceListener;
+import org.jrdf.util.boundary.RegexMatcher;
+import org.jrdf.util.boundary.RegexMatcherFactory;
 
 import java.util.regex.Pattern;
 
 public class PrefixParserImpl implements PrefixParser {
     private static final Pattern PREFIX_REGEX = Pattern.compile("\\p{Blank}*" +
-                    "@prefix\\p{Blank}+" +
-                    "([a-zA-Z][\\x20-\\x7E]*)?:" +
-                    "\\p{Blank}+\\<(([\\x20-\\x7E]+?))\\>\\p{Blank}*\\.\\p{Blank}*");
+        "@prefix\\p{Blank}+" +
+        "([a-zA-Z][\\x20-\\x7E]*)?:" +
+        "\\p{Blank}+\\<(([\\x20-\\x7E]+?))\\>\\p{Blank}*\\.\\p{Blank}*");
     private static final int PREFIX_GROUP = 1;
     private static final int URI_GROUP = 2;
     private final RegexMatcherFactory regexMatcherFactory;
