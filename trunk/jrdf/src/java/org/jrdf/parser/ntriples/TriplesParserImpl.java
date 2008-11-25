fdfd2341d0854dbfe489a9a5358e42065c6c7e43
newmana <newmana@f2e0c355-1f10-0410-b112-b8ac4569a347>
2008-11-25 01:41:21 +0000

    Blank node support.

--- trunk/jrdf/src/java/org/jrdf/parser/ntriples/TriplesParserImpl.java
+++ trunk/jrdf/src/java/org/jrdf/parser/ntriples/TriplesParserImpl.java
@@ -89,8 +89,6 @@ public class TriplesParserImpl implements TriplesParser {
         if (tripleRegexMatcher.matches()) {
             final Triple triple = tripleParser.parseTriple(tripleRegexMatcher);
             sh.handleStatement(triple.getSubject(), triple.getPredicate(), triple.getObject());
-        } else {
-            throw new IllegalArgumentException("Cannot parse line: " + line);
         }
     }
 
