303dabdcdb3e0daa9ec9d03c24916e5527333286
newmana <newmana@f2e0c355-1f10-0410-b112-b8ac4569a347>
2008-11-26 04:59:52 +0000

    Almost n3.

--- jrdf/test/groovy/org/jrdf/util/test/RdfBuilderUnitTest.groovy
+++ jrdf/test/groovy/org/jrdf/util/test/RdfBuilderUnitTest.groovy
@@ -9,7 +9,7 @@ class RdfBuilderUnitTest extends GroovyTestCase {
         Graph graph = TestJRDFFactory.factory.graph
         def rdf = new RdfBuilder(graph)
         def urn = rdf.namespace("urn", "http://this/is/not/a/urn#");
-        def xsd = rdf.namespace("xsd", "http://www.w3.org/2001/XMLSchema#");
+        rdf.namespace("xsd", "http://www.w3.org/2001/XMLSchema#");
 
         // Normal triples
         rdf.'urn:foo1' 'urn:bar':'urn:baz'
@@ -58,6 +58,5 @@ class RdfBuilderUnitTest extends GroovyTestCase {
         }
 
         assertEquals(16, graph.getNumberOfTriples())
-        println("Got " + graph)
     }
 }
