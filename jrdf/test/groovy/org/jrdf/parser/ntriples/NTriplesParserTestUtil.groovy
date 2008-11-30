0229ab0f8a7607c8e7f960d0e99ee823588ae27c
newmana <newmana@f2e0c355-1f10-0410-b112-b8ac4569a347>
2008-11-30 02:21:53 +0000

    Some coverage for n3 parser.

--- jrdf/test/groovy/org/jrdf/parser/ntriples/NTriplesParserTestUtil.groovy
+++ jrdf/test/groovy/org/jrdf/parser/ntriples/NTriplesParserTestUtil.groovy
@@ -1,5 +1,6 @@
 package org.jrdf.parser.ntriples
 
+import org.jrdf.TestJRDFFactory
 import static org.jrdf.graph.AnyObjectNode.ANY_OBJECT_NODE
 import static org.jrdf.graph.AnyPredicateNode.ANY_PREDICATE_NODE
 import static org.jrdf.graph.AnySubjectNode.ANY_SUBJECT_NODE
@@ -7,13 +8,11 @@ import org.jrdf.graph.Graph
 import org.jrdf.graph.Triple
 import org.jrdf.parser.GraphStatementHandler
 import org.jrdf.parser.ParserBlankNodeFactory
-import org.jrdf.util.test.RdfBuilder
-import org.jrdf.TestJRDFFactory
 import org.jrdf.parser.RDFEventReader
-import org.jrdf.parser.RDFInputFactory
+import org.jrdf.parser.n3.N3ParserFactoryImpl
+import org.jrdf.util.test.RdfBuilder
 
 class NTriplesParserTestUtil {
-
 
 
     private NTriplesParserTestUtil() {
@@ -35,7 +34,18 @@ class NTriplesParserTestUtil {
     static Set<Triple> parseNTriplesFile(InputStream input, Graph graph, ParserBlankNodeFactory factory) {
         def parserFactory = new NTriplesParserFactoryImpl()
         def ntriplesParser = parserFactory.createParser(graph, factory)
-        def parser = new LineParserImpl(ntriplesParser)
+        return parseFile(ntriplesParser, input, graph, factory);
+    }
+
+    static Set<Triple> parseN3File(InputStream input, Graph graph, ParserBlankNodeFactory factory) {
+        def parserFactory = new N3ParserFactoryImpl()
+        def ntriplesParser = parserFactory.createParser(graph, factory)
+        return parseFile(ntriplesParser, input, graph, factory);
+    }
+
+    static Set<Triple> parseFile(LineHandler lineHandler, InputStream input, Graph graph,
+        ParserBlankNodeFactory factory) {
+        def parser = new LineParserImpl(lineHandler)
         parser.setStatementHandler(new GraphStatementHandler(graph))
         parser.parse(input, "foo")
         def actualResults = new HashSet<Triple>()
@@ -70,39 +80,39 @@ class NTriplesParserTestUtil {
         def rdf = new RdfBuilder(newGraph)
         rdf.namespace("eg", "http://example.org/")
         rdf.namespace("rdfs", "http://www.w3.org/2000/01/rdf-schema#")
-        rdf."eg:resource1" "eg:property":"eg:resource2"
-        rdf."_:anon" "eg:property":"eg:resource2"
-        rdf."eg:resource2" "eg:property":"_:anon"
+        rdf."eg:resource1" "eg:property": "eg:resource2"
+        rdf."_:anon" "eg:property": "eg:resource2"
+        rdf."eg:resource2" "eg:property": "_:anon"
         (3..6).each {
-            rdf."eg:resource$it" "eg:property":"eg:resource2"
+            rdf."eg:resource$it" "eg:property": "eg:resource2"
         }
-        rdf."eg:resource7" "eg:property":'"simple literal"'
-        rdf."eg:resource8" "eg:property":'"backslash:\\"'
-        rdf."eg:resource9" "eg:property":'"dquote:\""'
-        rdf."eg:resource10" "eg:property":'"newline:\\n"'
-        rdf."eg:resource11" "eg:property":'"return\\r"'
-        rdf."eg:resource12" "eg:property":'"tab:\\t"'
-        rdf."eg:resource13" "eg:property":"eg:resource2"
-        rdf."eg:resource14" "eg:property":'"x"'
-        rdf."eg:resource15" "eg:property":"_:anon"
-        rdf."eg:resource16" "eg:property":'"\\u00E9"'
-        rdf."eg:resource17" ("eg:property":['"\\u20AC"', '"\\uD800\\uDC00"', '"\\uD84C\\uDFB4"', '"\\uDBFF\\uDFFF"'])
-        rdf."eg:resource21" "eg:property":'""^^rdfs:XMLLiteral'
-        rdf."eg:resource22" "eg:property":'" "^^rdfs:XMLLiteral'
-        rdf."eg:resource23" ("eg:property":['"x"^^rdfs:XMLLiteral', '"\""^^rdfs:XMLLiteral'] )
-        rdf."eg:resource24" "eg:property":'"<a></a>"^^rdfs:XMLLiteral'
-        rdf."eg:resource25" "eg:property":'"a <b></b>"^^rdfs:XMLLiteral'
-        rdf."eg:resource26" ("eg:property":['"a <b></b> c"^^rdfs:XMLLiteral', '"a\\n<b></b>\\nc"^^rdfs:XMLLiteral'])
-        rdf."eg:resource27" "eg:property":'"chat"^^rdfs:XMLLiteral'
-        rdf."eg:resource30" "eg:property":'"chat"@fr'
-        rdf."eg:resource31" "eg:property":'"chat"@en'
-        rdf."eg:resource32" "eg:property":'"abc"^^eg:datatype1'
+        rdf."eg:resource7" "eg:property": '"simple literal"'
+        rdf."eg:resource8" "eg:property": '"backslash:\\"'
+        rdf."eg:resource9" "eg:property": '"dquote:\""'
+        rdf."eg:resource10" "eg:property": '"newline:\\n"'
+        rdf."eg:resource11" "eg:property": '"return\\r"'
+        rdf."eg:resource12" "eg:property": '"tab:\\t"'
+        rdf."eg:resource13" "eg:property": "eg:resource2"
+        rdf."eg:resource14" "eg:property": '"x"'
+        rdf."eg:resource15" "eg:property": "_:anon"
+        rdf."eg:resource16" "eg:property": '"\\u00E9"'
+        rdf."eg:resource17"("eg:property": ['"\\u20AC"', '"\\uD800\\uDC00"', '"\\uD84C\\uDFB4"', '"\\uDBFF\\uDFFF"'])
+        rdf."eg:resource21" "eg:property": '""^^rdfs:XMLLiteral'
+        rdf."eg:resource22" "eg:property": '" "^^rdfs:XMLLiteral'
+        rdf."eg:resource23"("eg:property": ['"x"^^rdfs:XMLLiteral', '"\""^^rdfs:XMLLiteral'])
+        rdf."eg:resource24" "eg:property": '"<a></a>"^^rdfs:XMLLiteral'
+        rdf."eg:resource25" "eg:property": '"a <b></b>"^^rdfs:XMLLiteral'
+        rdf."eg:resource26"("eg:property": ['"a <b></b> c"^^rdfs:XMLLiteral', '"a\\n<b></b>\\nc"^^rdfs:XMLLiteral'])
+        rdf."eg:resource27" "eg:property": '"chat"^^rdfs:XMLLiteral'
+        rdf."eg:resource30" "eg:property": '"chat"@fr'
+        rdf."eg:resource31" "eg:property": '"chat"@en'
+        rdf."eg:resource32" "eg:property": '"abc"^^eg:datatype1'
     }
 
     static def addN3ValuesToGraph(Graph newGraph) {
         def rdf = new RdfBuilder(newGraph)
         rdf.namespace("rdf", "http://www.w3.org/1999/02/22-rdf-syntax-ns#")
-        rdf."_:references1" "rdf:type":"rdf:Bag"
-        rdf."_:references1" "rdf:_1":"<http://localhost/misc/UnknownDocument>"
+        rdf."_:references1" "rdf:type": "rdf:Bag"
+        rdf."_:references1" "rdf:_1": "<http://localhost/misc/UnknownDocument>"
     }
 }
